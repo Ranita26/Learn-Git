8d73829d8c66fd1cfd140b80272f70a4e7e27ef1
HP <HP@DESKTOP-79VLJDD>
2022-06-18 15:48:01 +0530

    change

--- Learn/src/myPkg/dummy.java
+++ Learn/src/myPkg/dummy.java
@@ -5,6 +5,7 @@ public class dummy {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("Hello");
+       System.out.println(System.getProperties());
 	}
 
 }
